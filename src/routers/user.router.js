6fd070b54204e8ff532f4d2276c306f112113fd0
Hannan Khan <hannan.khan098@gmail.com>
2025-07-04 20:50:28 +0530

    Add users/logoutAll route

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -40,6 +40,16 @@ router.post("/users/logout", auth, async (req, res) => {
   }
 });
 
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.payload.tokens = [];
+    await req.payload.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.payload);
 });
