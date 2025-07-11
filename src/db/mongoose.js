0c3425e0e4de6cdee0a785cf381de5f5667f0fee
Hannan Khan <hannan.khan098@gmail.com>
2025-07-12 01:57:55 +0530

    Explicitly set ssl and tls options

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    ssl: true,
+    tlsAllowInvalidCertificates: false,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
